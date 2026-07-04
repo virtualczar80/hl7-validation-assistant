337dfbe9dbf779eac3f7ea9bedf0d4b112c73ede
virtualczar80 <virtualczar@gmail.com>
2026-07-04 02:51:46 -0700

    Add API home endpoint

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from app.models import HL7Request
 from app.service import process
 
-
 app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "application": "HL7 Validation Assistant",
+        "status": "Running",
+        "docs": "/docs"
+    }
 
 
 @app.post("/validate")
 def validate(request: HL7Request):
     return process(request.hl7)
-
-
